92a882852eff62b8c0e6665a23a5c666b64c3fdd
Nour El Gafy <nour-elgafy@outlook.com>
2020-08-28 19:04:26 +0200

    finished exporting to excel removed printing to pdf

--- Assets/Views/ShowExpiryNotificationWindow.xaml.cs
+++ Assets/Views/ShowExpiryNotificationWindow.xaml.cs
@@ -1,8 +1,15 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
+using Assets.Helpers;
 using Assets.Models.Dtos;
+using Microsoft.Office.Interop.Excel;
+using Application = Microsoft.Office.Interop.Excel.Application;
+using DataTable = System.Data.DataTable;
+using Window = System.Windows.Window;
 
 namespace Assets.Views
 {
@@ -26,20 +33,44 @@ namespace Assets.Views
             window.Show();
         }
 
-        private void PrintBTN_OnClick(object sender, RoutedEventArgs e)
+        private void ExcelBTN_OnClick(object sender, RoutedEventArgs e)
         {
-            PrintDialog dlg = new PrintDialog();
+            string customExcelSavingPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\" +
+                                           "AssetsReport.xlsx";
+            GenerateExcel(ExcelHelpers.ConvertToDataTable(new List<AssetDto>(AssetGridDataSource)),
+                customExcelSavingPath);
+        }
 
-            Window currentMainWindow = Application.Current.MainWindow;
+        public static void GenerateExcel(DataTable dataTable, string path)
+        {
+            var dataSet = new DataSet();
+            dataSet.Tables.Add(dataTable);
 
-            Application.Current.MainWindow = this;
-
-            if ((bool)dlg.ShowDialog().GetValueOrDefault())
+            // create a excel app along side with workbook and worksheet and give a name to it  
+            var excelApp = new Application();
+            Workbook excelWorkBook = excelApp.Workbooks.Add();
+            _Worksheet xlWorksheet = excelWorkBook.Sheets[1];
+            Range xlRange = xlWorksheet.UsedRange;
+            foreach (DataTable table in dataSet.Tables)
             {
-                Application.Current.MainWindow = currentMainWindow; // do it early enough if the 'if' is entered
-                dlg.PrintTicket.PageOrientation = System.Printing.PageOrientation.Landscape;
-                dlg.PrintVisual(this, "Certificate");
+                //Add a new worksheet to workbook with the Datatable name  
+                Worksheet excelWorkSheet = excelWorkBook.Sheets.Add();
+                excelWorkSheet.Name = table.TableName;
+
+                // add all the columns  
+                for (int i = 1; i < table.Columns.Count + 1; i++)
+                    excelWorkSheet.Cells[1, i] = table.Columns[i - 1].ColumnName;
+
+                // add all the rows  
+                for (int j = 0; j < table.Rows.Count; j++)
+                for (int k = 0; k < table.Columns.Count; k++)
+                    excelWorkSheet.Cells[j + 2, k + 1] = table.Rows[j].ItemArray[k].ToString();
             }
+
+            // excelWorkBook.Save(); -> this is save to its default location  
+            excelWorkBook.SaveAs(path); // -> this will do the custom  
+            excelWorkBook.Close();
+            excelApp.Quit();
         }
     }
 }
